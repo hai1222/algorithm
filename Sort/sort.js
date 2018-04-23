c8631b3ca4b1440cc3c6ea1f6bdc48989611d23f
ShawnMao <1283293845@qq.com>
2018-04-23 23:04:39 +0800

    modify

--- Sort/sort.js
+++ Sort/sort.js
@@ -7,6 +7,7 @@ function CArray(numElements) {
     this.clear = clear;
     this.setData = setData;
     this.swap = swap;
+    this.bubbleSort = bubbleSort;
 
     for (var i = 0; i < numElements; ++i) {
         this.dataStore[i] = i;
@@ -19,3 +20,48 @@ function setData() {
     }
 }
 
+function clear() {
+    for (var i = 0; i < this.dataStore.length; ++i) {
+        this.dataStore[i] = 0;
+    }
+}
+
+function insert(element) {
+    this.dataStore[this.pos++] = element;
+}
+
+function toString() {
+    var retstr = "";
+    for (var i = 0; i < this.dataStore.length; ++i) {
+        retstr += this.dataStore[i] + " ";
+        if (i > 0 && i % 10 == 9) {
+            retstr += "\n";
+        }
+    }
+    return retstr;
+}
+
+function swap(arr, index1, index2) {
+    var temp = arr[index1];
+    arr[index1] = arr[index2]
+    arr[index2] = temp;
+}
+
+function bubbleSort() {
+    var numElements = this.dataStore.length;
+    var temp;
+    for (var outer = numElements; outer >= 2; --outer) {
+        for (var inner = 0; inner <= outer - 1; ++inner) {
+            if (this.dataStore[inner] > this.dataStore[inner + 1]) {
+                this.swap(this.dataStore, inner, inner + 1);
+            }
+        }
+    }
+}
+
+var numElements = 10;
+var myNums = new CArray(numElements);
+myNums.setData();
+console.log(myNums.toString());
+myNums.bubbleSort();
+console.log(myNums.toString());
--- Sort/sort.php
+++ Sort/sort.php
@@ -1,1 +1,64 @@
 <?php
+
+class CArray {
+
+	public $dataStore = [];
+	public $pos = 0;
+	public $numElements;
+
+	public function __construct($numElements) {
+		$this->numElements = $numElements;
+	}
+
+	public function setData() {
+		for ($i = 0; $i < $this->numElements; ++$i) {
+			$this->dataStore[$i] = rand(1, $this->numElements);
+		}
+	}
+
+	public function clear() {
+		for ($i = 0; $i < count($this->dataStore); ++$i) {
+			$this->dataStore[$i] = 0;
+		}
+	}
+
+	public function insert($element) {
+		$this->dataStore[$this->pos++] = $element;
+	}
+
+	public function toString() {
+		$retstr = "";
+		for ($i = 0; $i < count($this->dataStore); ++$i) {
+			$retstr .= $this->dataStore[$i] . " ";
+			if ($i > 0 && $i % 10 == 9) {
+				$retstr .= "<br>";
+			}
+		}
+		return $retstr;
+	}
+
+	public function swap(&$arr, $index1, $index2) {
+		$temp = $arr[$index1];
+		$arr[$index1] = $arr[$index2];
+		$arr[$index2] = $temp;
+	}
+
+	public function bubbleSort() {
+		$numElements = count($this->dataStore);
+		$temp;
+		for ($outer = $numElements; $outer >= 2; --$outer) {
+			for ($inner = 0; $inner < $outer - 1; ++ $inner) {
+				if ($this->dataStore[$inner] > $this->dataStore[$inner + 1]) {
+					$this->swap($this->dataStore, $inner, $inner + 1);
+				}
+			}
+		}
+	}
+}
+
+$numElements = 10;
+$myNums = new CArray($numElements);
+$myNums->setData();
+echo $myNums->toString();
+$myNums->bubbleSort();
+echo $myNums->toString();
