7ad02a85621c2dd53486abe5459f454333574be1
ShawnMao <1283293845@qq.com>
2018-05-07 10:29:36 +0800

    modify

--- Sort/sort.js
+++ Sort/sort.js
@@ -14,6 +14,8 @@ function CArray(numElements) {
     this.shellsort1 = shellsort1;
     this.gaps = [5, 3, 1];
     this.setGaps = setGaps;
+    this.mergeSort = mergeSort;
+    this.mergeArrays = mergeArrays;
 
     for (var i = 0; i < numElements; ++i) {
         this.dataStore[i] = i;
@@ -124,3 +126,60 @@ function shellsort1() {
 function setGaps(arr) {
     this.gaps = arr;
 }
+
+function mergeSort(arr) {
+    debugger;
+    if (arr.length < 2) {
+        return;
+    }
+    var step = 1;
+    var left, right;
+    while (step < arr.length) {
+        left = 0;
+        right = step;
+        while (right + step <= arr.length) {
+            this.mergeArrays(arr, left, left+step, right, right+step);
+            left = right + step;
+            right = left + step;
+        }
+        if (right < arr.length) {
+            this.mergeArrays(arr, left, left+step, right, arr.length);
+        }
+        step *= 2;
+    }
+}
+
+function mergeArrays(arr, startLeft, stopLeft, startRight, stopRight) {
+    var rightArr = new Array(stopRight - startRight + 1);
+    var leftArr = new Array(stopLeft - startLeft + 1);
+    k = startRight;
+    for (var i = 0; i < (rightArr.length - 1); ++i) {
+        rightArr[i] = arr[k];
+        ++k;
+    }
+    k = startLeft;
+    for (var i = 0; i < (leftArr.length - 1); ++i) {
+        leftArr[i] = arr[k];
+        ++k;
+    }
+    rightArr[rightArr.length-1] = Infinity;
+    leftArr[leftArr.length-1] = Infinity;
+    var m = 0;
+    var n = 0;
+    for (var k = startLeft; k < stopRight; ++k) {
+        if (leftArr[m] <= rightArr[n]) {
+            arr[k] = leftArr[m];
+            m++;
+        } else {
+            arr[k] = rightArr[n];
+            n++;
+        }
+    }
+    console.log("left array - " + leftArr);
+    console.log("right array - " + rightArr);
+}
+
+var nums = [6, 10, 1, 9, 4, 8, 2, 7, 3, 5];
+console.log(nums);
+mergeSort(nums);
+console.log(nums);
